d054cb7516f87073c75dc8062a873003f8bbbf95
Shadoweyes75 <cperez4@email.arizona.edu>
2021-02-10 22:11:24 -0700

    question 4

--- practicum/week-2/question4.js
+++ practicum/week-2/question4.js
@@ -12,10 +12,13 @@ The letters in A are guaranteed distinct, and all characters in A and B are lett
 
 var howManyCommon = function(A, B) {
     const Alist = A.split("");
+    const Blist = B.split("");
     let sameLetters = 0;
     for(var i = 0; i < Alist.length; i++){
-        if((B.indexOf(Alist[i])) > -1){
-            sameLetters ++;
+        for(var x = 0; x < Blist.length; x++){
+            if(Alist[i] == Blist[x]){
+                sameLetters ++;
+            }
         }
     }
     return sameLetters;
