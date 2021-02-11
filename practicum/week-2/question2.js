ff16edc1b9f102e6cfd1d2bce403148529c1a58b
Shadoweyes75 <cperez4@email.arizona.edu>
2021-02-10 21:44:59 -0700

    question 2

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -16,7 +16,6 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
                 const curCookiesEaten = cookies[i];
                 balanceCookies += mostCookiesEaten - curCookiesEaten;
         }
-        return typeof(balanceCookies);
         if(balanceCookies == extraCookies){
                 return true;
         }else if(balanceCookies < extraCookies){
